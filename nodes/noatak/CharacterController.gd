0487a2487588b64f4b7808c9d12857bc94c11e73
noatakak <noatak.post@gmail.com>
2023-08-01 21:34:06 -0700

    imported 1st person controller and made ui mock up

---
+++ nodes/noatak/CharacterController.gd
@@ -0,0 +1,72 @@
+class_name Player extends CharacterBody3D
+
+@export_category("Player")
+@export_range(1, 35, 1) var speed: float = 10 # m/s
+@export_range(10, 400, 1) var acceleration: float = 100 # m/s^2
+@export_range(0.1, 3.0, 0.1, "or_greater") var camera_sens: float = 1
+
+var jumping: bool = false
+var mouse_captured: bool = false
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var move_dir: Vector2 # Input direction for movement
+var look_dir: Vector2 # Input direction for look/aim
+
+var walk_vel: Vector3 # Walking velocity 
+var grav_vel: Vector3 # Gravity velocity 
+
+@onready var camera: Camera3D = $Camera
+@onready var anim = $"Camera/AnimationPlayer"
+
+func _ready() -> void:
+	capture_mouse()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		look_dir = event.relative * 0.001
+		if mouse_captured: _rotate_camera()
+	if Input.is_action_just_pressed("pause"):
+		$Camera/RayCast3D/prompt.visible = false
+		get_tree().paused = true
+		#PauseMenu.fullscreen = Game.fullscreen
+		#PauseMenu.volume = Game.volume_level
+		#PauseMenu.show()
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(delta: float) -> void:
+	if mouse_captured: _handle_joypad_camera_rotation(delta)
+	velocity = _walk(delta) + _gravity(delta)
+	if velocity != Vector3():
+		anim.play("headbob")
+	move_and_slide()
+
+func capture_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	mouse_captured = true
+
+func release_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	mouse_captured = false
+
+func _rotate_camera(sens_mod: float = 1.0) -> void:
+	camera.rotation.y -= look_dir.x * camera_sens * sens_mod
+	camera.rotation.x = clamp(camera.rotation.x - look_dir.y * camera_sens * sens_mod, -1.5, 1.5)
+
+func _handle_joypad_camera_rotation(delta: float, sens_mod: float = 1.0) -> void:
+	var joypad_dir: Vector2 = Input.get_vector("look_left","look_right","look_up","look_down")
+	if joypad_dir.length() > 0:
+		look_dir += joypad_dir * delta
+		_rotate_camera(sens_mod)
+		look_dir = Vector2.ZERO
+
+func _walk(delta: float) -> Vector3:
+	move_dir = Input.get_vector("left", "right", "forward", "backward")
+	var _forward: Vector3 = camera.transform.basis * Vector3(move_dir.x, 0, move_dir.y)
+	var walk_dir: Vector3 = Vector3(_forward.x, 0, _forward.z).normalized()
+	walk_vel = walk_vel.move_toward(walk_dir * speed * move_dir.length(), acceleration * delta)
+	return walk_vel
+
+func _gravity(delta: float) -> Vector3:
+	grav_vel = Vector3.ZERO if is_on_floor() else grav_vel.move_toward(Vector3(0, velocity.y - gravity, 0), gravity * delta)
+	return grav_vel
---
+++ nodes/noatak/RayCast3D.gd
@@ -0,0 +1,26 @@
+extends RayCast3D
+
+var sum
+
+func _ready():
+	sum = 2
+
+func _process(_delta):
+	sum += _delta
+	if is_colliding() and not DialogBox.visible and sum > 2:
+		$prompt.visible = truesdawad
+		if Input.is_action_just_pressed("interact"):
+			sum = 0
+			var coll = get_collider()
+			$prompt.visible = false
+			if coll.has_meta("destination"):
+				$prompt.visible = false
+				if coll.get_meta("destination") != "res://levels/level7/level_7.tscn":
+					LevelTransition.change_level(true, false, true, coll.get_meta("destination"), "")
+				else:
+					LevelTransition.change_level(true, true, true, coll.get_meta("destination"), "scientist")
+			elif coll.has_meta("dialog_id"):
+				$prompt.visible = false
+				DialogBox.open_dialog(coll.get_meta("dialog_id"))
+	else:
+		$prompt.visible = false
