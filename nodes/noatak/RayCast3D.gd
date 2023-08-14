758e7622763cfcd53e031c8c08b4747554154ba6
noatakak <noatak.post@gmail.com>
2023-08-13 18:01:38 -0700

    added sand texture and started work on dunk ai

--- nodes/noatak/RayCast3D.gd
+++ nodes/noatak/RayCast3D.gd
@@ -18,6 +18,8 @@ func _process(_delta):
 				if coll.get_meta("type") == "air":
 					AudioManager.play_air_refill()
 					coll.get_parent().get_parent().get_parent().get_parent().o2_level += 60
+					if coll.get_parent().get_parent().get_parent().get_parent().o2_level > 100:
+						coll.get_parent().get_parent().get_parent().get_parent().o2_level = 100
 					coll.get_parent().get_parent().visible = false
 				if coll.get_meta("type") == "power":
 					AudioManager.play_battery_insert()
